7ca2b746fe0463f6b263715b66a22cd2cc2c6aae
Peter Hunt <peter.s.hunt@gmail.com>
2018-09-09 00:26:26 -0700

    comment out welcome explorers function for now

--- functions/index.js
+++ functions/index.js
@@ -44,41 +44,44 @@ exports.welcomeTaddler = functions.firestore
 			.catch(err => console.log(err) )
 	})
 
-exports.welcomeExplorer = functions.firestore
-	.document('taddlers/{taddlerId}')
-	.onCreate(event => {
-
-	const taddlerId = event.params.taddlerId;
-
-	const db = admin.firestore()
-
-	return db.collection('taddlers').doc(taddlerId)
-			.get()
-			.then(doc => {
-
-				const user = doc.data()
-
-				const msg = {
-					to: user.details.email,
-					from: 'teamtattlers@gmail.com',
-					// subject: 'Welcome to Tattle', 
-					// subject can be created on the template
-
-					templateId: 'd-f6a9855fd4c0460ca94165da18c0a17a',
-					// substitutionWrappers: ['{{', '}}'],
-					substitutions: {
-						name: user.name
-						//other custom properties here
-					}
 
 
-				};
+// need to understand how we are tracking info on users.  
+// exports.welcomeExplorer = functions.firestore
+// 	.document('taddlers/')
+// 	.onCreate(event => {
+
+// 	const taddlerId = event.params.taddlerId;
+
+// 	const db = admin.firestore()
+
+// 	return db.collection('taddlers').doc(taddlerId)
+// 			.get()
+// 			.then(doc => {
+
+// 				const user = doc.data()
+
+// 				const msg = {
+// 					to: user.details.email,
+// 					from: 'teamtattlers@gmail.com',
+// 					// subject: 'Welcome to Tattle', 
+// 					// subject can be created on the template
+
+// 					templateId: 'd-8b9dcfeb159045d590249f6f68896e6b',
+// 					// substitutionWrappers: ['{{', '}}'],
+// 					substitutions: {
+// 						name: user.name
+// 						//other custom properties here
+// 					}
+
+
+// 				};
 				
-				return sgMail.send(msg)
-			})
-			.then(() => console.log('email sent!') )
-			.catch(err => console.log(err) )
-	})
+// 				return sgMail.send(msg)
+// 			})
+// 			.then(() => console.log('email sent!') )
+// 			.catch(err => console.log(err) )
+// 	})
 
 exports.bookingConfirmation = functions.firestore
 	.document('bookings/{bookingId}/status/{confirmationStatus}')
